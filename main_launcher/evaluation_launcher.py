32e991dc195627e234b13ddf2a6569ba79657932
tanguis <tangui.steimetz@ecole.ensicaen.fr>
2023-07-21 11:40:32 +0200

    add a better evluation, still ~~

--- main_launcher/evaluation_launcher.py
+++ main_launcher/evaluation_launcher.py
@@ -1,13 +1,15 @@
 import base64
+import csv
 import time
 from pathlib import Path
-from typing import Dict, Tuple, Optional, Generator
+from typing import Dict, Tuple, Optional, Generator, List
 
 import cv2
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
+from tqdm import tqdm
 
 import models
 from bench.Evaluation import ModelEvaluator
@@ -42,8 +44,9 @@ def frame_to_base64(frame_path: Path) -> str:
     return base64_image
 
 
-def test_sample_base64_iterator(df_path: Path, root: Path, distribution: str, seed: int) -> Generator[
-    Tuple[str, bool], None, None]:
+def test_sample_base64_iterator(
+        df_path: Path, root: Path, distribution: str, seed: int
+) -> Generator[Tuple[str, bool], None, None]:
     df = pd.read_pickle(df_path)
     dataframe = balance_dataframe(df)
     split = distribution.split('-')
@@ -56,9 +59,10 @@ def test_sample_base64_iterator(df_path: Path, root: Path, distribution: str, se
         yield frame_to_base64(root.joinpath(frame_line[0])), frame_line[1]['label']
 
 
-def evaluate_model(root_face_folder: Path, df_faces_path: Path, arch: str, input_shape: Tuple[int, int, int],
-                   output_folder: Path, distribution: str, seed: int, yunet_model_path: Path,
-                   model_name: Optional[str]) -> Tuple[list, list, float]:
+def evaluate_model(
+        root_face_folder: Path, df_faces_path: Path, arch: str, input_shape: Tuple[int, int, int],
+        output_folder: Path, distribution: str, seed: int, yunet_model_path: Path, model_name: Optional[str]
+) -> Tuple[List[int], List[int], float, List[float], List[float]]:
     yunet = load_face_detection_model(yunet_model_path, input_size=(input_shape[0], input_shape[1]))
 
     model = models.import_model(arch)(
@@ -74,9 +78,11 @@ def evaluate_model(root_face_folder: Path, df_faces_path: Path, arch: str, input
     sum_time = 0
     y_true = []
     y_pred = []
+    yhat_0 = []
+    yhat_1 = []
 
     print("Evaluating...")
-    for frame, label in test_sample_base64_iterator(df_faces_path, root_face_folder, distribution, seed):
+    for frame, label in tqdm(test_sample_base64_iterator(df_faces_path, root_face_folder, distribution, seed)):
         start = time.time()
         yhat = evaluator.true_evaluate(frame)
         end = time.time()
@@ -87,7 +93,10 @@ def evaluate_model(root_face_folder: Path, df_faces_path: Path, arch: str, input
         y_true.append(int(label))
         y_pred.append(int(is_prediction_deepfake(yhat)))
 
-    return y_true, y_pred, sum_time / count
+        yhat_0.append(yhat[0][0])
+        yhat_1.append(yhat[0][1])
+
+    return y_true, y_pred, sum_time / count, yhat_0, yhat_1
 
 
 def plot_confusion_matrix(y_true: list, y_pred: list, arch: str, shape: Tuple[int, int, int]) -> None:
@@ -100,6 +109,16 @@ def plot_confusion_matrix(y_true: list, y_pred: list, arch: str, shape: Tuple[in
     plt.ylabel('True Label')
     plt.title(f'Confusion Matrix: {arch} - {shape}')
     plt.show()
+
+
+def save_yhat(yhat_0: List[float], yhat_1: List[float], sum_time: float, csv_path: Path) -> None:
+    data = list(zip(yhat_0, yhat_1))
+
+    with open(csv_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=';')
+        csv_writer.writerow(['Yhat Fake', 'Yhat Real'])
+        csv_writer.writerows(data)
+        csv_writer.writerow(['', '', 'Sum Time', sum_time])
 
 
 def launch_evaluation(
@@ -118,9 +137,12 @@ def launch_evaluation(
     workspace.mkdir(exist_ok=True)
     logger = Logger()
 
-    y_true, y_pred, avg_prediction_time = evaluate_model(root_face_folder, df_faces_path, arch, input_shape,
-                                                         output_folder, distribution, seed, yunet_model_path,
-                                                         model_name)
+    y_true, y_pred, avg_prediction_time, yhat_0, yhat_1 = evaluate_model(
+        root_face_folder, df_faces_path, arch, input_shape, output_folder,
+        distribution, seed, yunet_model_path, model_name
+    )
 
     plot_confusion_matrix(y_true, y_pred, arch, input_shape)
     print(f"Average time to make a prediction: {avg_prediction_time}")
+
+    save_yhat(yhat_0, yhat_1, avg_prediction_time, workspace.joinpath('yhat.csv'))
